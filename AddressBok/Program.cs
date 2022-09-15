85b478b1a6229adad83c287f542fd0f890b0054e
kavyabs11 <kavyasomshekar11@gmail.com>
2022-09-15 07:17:28 -0700

    Create contact

--- AddressBok/Program.cs
+++ AddressBok/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace AddressBok
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using AddressBok;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AddressBook;
+
+Console.WriteLine("Welcome to Address Book Program in Address Book Main class on Master Branch");
+Console.WriteLine("Enter the details to create a contact");
+Address obj = new Address();
+obj.create();
+obj.display();
